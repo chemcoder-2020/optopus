e8a14e68719a4f3df5c4dd3463053abe521dc958
chemcoder-2020 <59574987+chemcoder-2020@users.noreply.github.com>
2024-10-30 22:52:00 -0700

    fix: Update version and improve SchwabOptionOrder submit_exit method

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='optopus',
-    version='0.5.11',
+    version='0.5.12',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     author='Huy Nguyen',
