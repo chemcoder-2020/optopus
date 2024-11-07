740611932dfb6647faa37f8acb0953f40cde2b13
chemcoder-2020 <59574987+chemcoder-2020@users.noreply.github.com>
2024-11-07 09:29:03 -0800

    build: Bump version to 0.6.0-dev13

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='optopus',
-    version='0.6.0-dev12',
+    version='0.6.0-dev13',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     author='Huy Nguyen',
