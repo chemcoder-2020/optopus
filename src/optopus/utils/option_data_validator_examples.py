bb1567568d47988f33e4bb436d90ca7050b5293c
chemcoder-2020 <59574987+chemcoder-2020@users.noreply.github.com>
2024-12-16 12:22:16 -0800

    fix: Update example parquet file path

--- src/optopus/utils/option_data_validator_examples.py
+++ src/optopus/utils/option_data_validator_examples.py
@@ -1,9 +1,9 @@
 import pandas as pd
-from optopus.utils.option_data_validator import validate_option_data
+from option_data_validator import validate_option_data
 
 if __name__ == "__main__":
     # Replace 'sample_option_data.parquet' with the actual path to your parquet file
-    parquet_file_path = "sample_option_data.parquet"
+    parquet_file_path = "/Users/traderHuy/Library/CloudStorage/OneDrive-Personal/Documents/optopus-dev/data/SPY_2024-09-06 15-30.parquet"
     try:
         # Read the parquet file into a pandas DataFrame
         df = pd.read_parquet(parquet_file_path)
