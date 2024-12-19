1a9bb43c70030456560604cca3e78c6896f81c42
chemcoder-2020 <59574987+chemcoder-2020@users.noreply.github.com>
2024-12-18 20:34:50 -0800

    chore: bump version to 0.9.1-dev2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='optopus',
-    version='0.9.1-dev1',
+    version='0.9.1-dev2',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     author='Huy Nguyen',
