068b2c9bcc5e11dcc41691b6faff1e91ef486e10
chemcoder-2020 <59574987+chemcoder-2020@users.noreply.github.com>
2025-01-30 13:37:23 -0800

    feat: Add forecast models module

---
+++ src/optopus/decisions/forecast_models.py
@@ -0,0 +1,149 @@
+import warnings
+import pandas as pd
+from statsforecast import StatsForecast
+from statsforecast.models import (
+    ARIMA,
+    SeasonalExponentialSmoothingOptimized,
+    RandomWalkWithDrift,
+    AutoARIMA,
+    AutoCES,
+)
+from neuralforecast import NeuralForecast
+from neuralforecast.models import NBEATS
+
+
+class ForecastModels:
+    @staticmethod
+    def check_arima_trend(monthly_data, current_price, freq="M"):
+        """Check ARIMA forecast for upward trend"""
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", category=Warning)
+            forecaster = StatsForecast(
+                models=[ARIMA(order=(0, 1, 1), seasonal_order=(0, 1, 1))],
+                freq=freq,
+            )
+            sf_df = pd.DataFrame(
+                {
+                    "ds": list(monthly_data.index),
+                    "y": list(monthly_data.values),
+                    "unique_id": "1",
+                }
+            )
+            forecaster.fit(sf_df)
+            predictions = forecaster.predict(h=1)["ARIMA"]
+            return predictions.iloc[-1] > current_price
+        
+    @staticmethod
+    def check_autoarima_trend(monthly_data, current_price, freq="M"):
+        """Check AutoARIMA forecast for upward trend"""
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", category=Warning)
+            forecaster = StatsForecast(
+                models=[AutoARIMA()],
+                freq=freq,
+            )
+            sf_df = pd.DataFrame(
+                {
+                    "ds": list(monthly_data.index),
+                    "y": list(monthly_data.values),
+                    "unique_id": "1",
+                }
+            )
+            forecaster.fit(sf_df)
+            predictions = forecaster.predict(h=1)["AutoARIMA"]
+            return predictions.iloc[-1] > current_price
+    
+    @staticmethod
+    def check_autoces_trend(monthly_data, current_price, freq="M"):
+        """Check AutoCES forecast for upward trend"""
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", category=Warning)
+            forecaster = StatsForecast(
+                models=[AutoCES()],
+                freq=freq,
+            )
+            sf_df = pd.DataFrame(
+                {
+                    "ds": list(monthly_data.index),
+                    "y": list(monthly_data.values),
+                    "unique_id": "1",
+                }
+            )
+            forecaster.fit(sf_df)
+            predictions = forecaster.predict(h=1)["CES"]
+            return predictions.iloc[-1] > current_price
+
+    @staticmethod
+    def check_nbeats_trend(monthly_data):
+        """Check NBEATS forecast for upward trend"""
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", category=Warning)
+            forecaster = NeuralForecast(
+                models=[
+                    NBEATS(
+                        input_size=4,
+                        h=2,
+                        max_steps=100,
+                        enable_progress_bar=False,
+                        logger=False,
+                        enable_checkpointing=False,
+                    )
+                ],
+                freq="M",
+            )
+            nf_df = pd.DataFrame(
+                {
+                    "ds": list(monthly_data.index),
+                    "y": list(monthly_data),
+                    "unique_id": "1",
+                }
+            )
+            forecaster.fit(nf_df)
+            predictions = forecaster.predict()[forecaster.models[0].__str__()]
+            return predictions.iloc[1] - predictions.iloc[0] > 0
+
+    @staticmethod
+    def check_arima_trend_daily(daily_data, current_price):
+        """Check ARIMA forecast for upward trend using daily data"""
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", category=Warning)
+            forecaster = StatsForecast(
+                models=[ARIMA(order=(0, 1, 1), seasonal_order=(0, 1, 1))],
+                freq="D",
+            )
+            sf_df = pd.DataFrame(
+                {"ds": list(daily_data.index), "y": list(daily_data), "unique_id": "1"}
+            )
+            forecaster.fit(sf_df)
+            predictions = forecaster.predict(h=1)["ARIMA"]
+            return predictions.iloc[-1] > current_price
+
+    @staticmethod
+    def check_RWD_trend(monthly_data, current_price):
+        """Check Random Walk with Drift forecast for upward trend"""
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", category=Warning)
+            forecaster = StatsForecast(
+                models=[RandomWalkWithDrift()],
+                freq="M",
+            )
+            sf_df = pd.DataFrame(
+                {
+                    "ds": list(monthly_data.index),
+                    "y": list(monthly_data),
+                    "unique_id": "1",
+                }
+            )
+            forecaster.fit(sf_df)
+            predictions = forecaster.predict(h=1)["RWD"]
+            return predictions.iloc[-1] > current_price
+
+    @staticmethod
+    def check_SES_trend(monthly_data, current_price):
+        """Check Seasonal Exponential Smoothing forecast for upward trend"""
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", category=Warning)
+            forecaster = SeasonalExponentialSmoothingOptimized(season_length=1)
+            forecaster.fit(y=monthly_data.values)
+            predictions = forecaster.predict(h=1)["mean"]
+            return predictions[-1] > current_price
