906a75c4251125262e4bdd92f63a57ad718765b7
chemcoder-2020 <59574987+chemcoder-2020@users.noreply.github.com>
2025-02-16 22:33:42 -0800

    refactor: Move premiums list to manager context in MedianCalculator

--- src/optopus/trades/entry_conditions.py
+++ src/optopus/trades/entry_conditions.py
@@ -36,7 +36,6 @@ class MedianCalculator(EntryConditionChecker):
         self.window_size = window_size
         self.fluctuation = fluctuation
         self.method = method
-        self.premiums = []
         self.kwargs = kwargs
         if method == "ContinuousMedian":
             self.median_calculator = ContinuousMedian()
@@ -46,47 +45,54 @@ class MedianCalculator(EntryConditionChecker):
                 n_sigma=self.kwargs.get("n_sigma", 3),
                 k=self.kwargs.get("k", 1.4826),
                 max_iterations=self.kwargs.get("max_iterations", 5),
-                replace_with_na=self.kwargs.get("replace_with_na", False),
+                replace_with_na=True,
             )
-
-    def add_premium(self, mark):
-        if self.method == "ContinuousMedian":
-            self.median_calculator.add(mark)
-        self.premiums.append(mark)
-
-        if self.method == "ContinuousMedian":
-            if len(self.premiums) > self.window_size:
-                self.median_calculator.remove(self.premiums.pop(0))
-        else:
-            if len(self.premiums) > self.window_size + 1:
-                self.premiums.pop(0)
-
-    def get_median(self):
-        """
-        Get the current median of the rolling window.
-
-        Returns:
-            float: The current median.
-        """
-        if self.method == "ContinuousMedian":
-            return self.median_calculator.get_median()
-        else:
-            if len(self.premiums) < self.window_size + 1:
-                return 0
-            else:
-                return self.median_calculator.fit_transform(
-                    np.array(self.premiums)
-                ).flatten()[-1]
 
     def should_enter(self, strategy, manager, time) -> bool:
         bid = strategy.current_bid
         ask = strategy.current_ask
-        mark = (ask + bid) / 2
+        mark = (ask + bid) / 2# if bid != 0 else ask
 
-        self.add_premium(mark)
-        median_mark = self.get_median()
+        if not hasattr(manager, 'context'):
+            manager.context = {}
+        
+        if 'premiums' not in manager.context:
+            manager.context['premiums'] = []
 
-        return np.isclose(mark, median_mark, rtol=self.fluctuation) and np.isclose(
+        manager.context['premiums'].append(mark)
+        if self.method == "ContinuousMedian":
+            self.median_calculator.add(mark)
+
+        if self.method == "ContinuousMedian":
+            if len(manager.context['premiums']) > self.window_size:
+                self.median_calculator.remove(manager.context['premiums'].pop(0))
+        else:
+            if len(manager.context['premiums']) > self.window_size + 1:
+                manager.context['premiums'].pop(0)
+
+        if self.method == "ContinuousMedian":
+            filtered_mark = self.median_calculator.get_median()
+        else:
+            if len(manager.context['premiums']) < self.window_size + 1:
+                filtered_mark = 0
+            else:
+                filtered_mark = self.median_calculator.fit_transform(
+                    np.array(manager.context['premiums'])
+                ).flatten()[-1]
+
+        if filtered_mark == 0:
+            logger.warning(f"Filtered mark is 0. Probably not enough data for MedianCalculator's window size of {self.window_size}")
+            return False
+        elif filtered_mark == np.nan:
+            logger.warning(f"Filtered mark is NaN. The filter {self.method} has detected an outlying price. Returning False and replacing premium list with previous value.")
+            manager.context['premiums'][-1] = manager.context['premiums'][-2]
+            return False
+        
+        logger.info(
+            f"Passed MedianCalculator check. Filtered mark: {filtered_mark}, Bid: {bid}, Mark: {mark}, Fluctuation: {self.fluctuation}"
+        )
+
+        return np.isclose(mark, filtered_mark, rtol=self.fluctuation) and np.isclose(
             bid, mark, rtol=self.fluctuation
         )
 
