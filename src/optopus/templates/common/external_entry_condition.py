86d0508c36e165f78e92a7c2a090238ac626bebf
chemcoder-2020 <59574987+chemcoder-2020@users.noreply.github.com>
2025-02-24 19:51:24 -0800

    docs: Add commented example exit and entry condition templates

--- src/optopus/templates/common/external_entry_condition.py
+++ src/optopus/templates/common/external_entry_condition.py
@@ -15,40 +15,41 @@ from optopus.trades.external_entry_conditions import (
 import pandas as pd
 import pandas_ta as pt
 
+# Uncomment the following line to use the Awesome Entry
 
-class MyAwesomeEntry(ExternalEntryConditionChecker):
+# class MyAwesomeEntry(ExternalEntryConditionChecker):
 
-    def __init__(self, **kwargs):
-        self.linear_window = kwargs.get("linear_window", 14)
-        self.lag1 = kwargs.get("lag1", 5)
-        self.lag2 = kwargs.get("lag2", 10)
-        self.gk_window = kwargs.get("gk_window", 30)
-        self.yz_window = kwargs.get("yz_window", 30)
-        self.c2c_window = kwargs.get("c2c_window", 30)
-        self.zero_drift = kwargs.get("zero_drift", True)
-        self.ohlc = kwargs.get("ohlc")
-        
-        self.pipeline = CompositePipelineCondition(
-            LinearRegressionCheck(lag=self.linear_window)
-            * IndicatorStateCheck(
-                pt.ema, lag1=self.lag1, lag2=self.lag2
-            )
-            * GarmanKlassVolatilityDecreaseCheck(lag=self.gk_window)
-            * YangZhangVolatilityDecreaseCheck(lag=self.yz_window)
-            * CloseToCloseVolatilityDecreaseCheck(
-                lag=self.c2c_window,
-                zero_drift=self.zero_drift,
-            ),
-            self.ohlc,
-        )
+#     def __init__(self, **kwargs):
+#         self.linear_window = kwargs.get("linear_window", 14)
+#         self.lag1 = kwargs.get("lag1", 5)
+#         self.lag2 = kwargs.get("lag2", 10)
+#         self.gk_window = kwargs.get("gk_window", 30)
+#         self.yz_window = kwargs.get("yz_window", 30)
+#         self.c2c_window = kwargs.get("c2c_window", 30)
+#         self.zero_drift = kwargs.get("zero_drift", True)
+#         self.ohlc = kwargs.get("ohlc")
 
-    def should_enter(self, strategy, manager, time: pd.Timestamp) -> bool:
-        return self.pipeline.should_enter(strategy, manager, time)
+#         self.pipeline = CompositePipelineCondition(
+#             LinearRegressionCheck(lag=self.linear_window)
+#             * IndicatorStateCheck(
+#                 pt.ema, lag1=self.lag1, lag2=self.lag2
+#             )
+#             * GarmanKlassVolatilityDecreaseCheck(lag=self.gk_window)
+#             * YangZhangVolatilityDecreaseCheck(lag=self.yz_window)
+#             * CloseToCloseVolatilityDecreaseCheck(
+#                 lag=self.c2c_window,
+#                 zero_drift=self.zero_drift,
+#             ),
+#             self.ohlc,
+#         )
 
-    def __repr__(self):
-        return (
-            f"MyAwesomeEntry(linear_window={self.linear_window}, "
-            f"lag1={self.lag1}, lag2={self.lag2}, gk_window={self.gk_window}, "
-            f"yz_window={self.yz_window}, c2c_window={self.c2c_window}, "
-            f"zero_drift={self.zero_drift}, ohlc={self.ohlc})"
-        )
+#     def should_enter(self, strategy, manager, time: pd.Timestamp) -> bool:
+#         return self.pipeline.should_enter(strategy, manager, time)
+
+#     def __repr__(self):
+#         return (
+#             f"MyAwesomeEntry(linear_window={self.linear_window}, "
+#             f"lag1={self.lag1}, lag2={self.lag2}, gk_window={self.gk_window}, "
+#             f"yz_window={self.yz_window}, c2c_window={self.c2c_window}, "
+#             f"zero_drift={self.zero_drift}, ohlc={self.ohlc})"
+#         )
